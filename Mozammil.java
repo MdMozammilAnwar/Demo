5ce0a182834be8ad6723a68e4e905892f8f3b9d4
Md Mozammil Anwar <25384402+MdMozammilAnwar@users.noreply.github.com>
2019-09-17 01:02:08 -1200

    Update Mozammil.java
    cerly bracess has been added and one new method m1() has been defines

--- Mozammil.java
+++ Mozammil.java
@@ -3,4 +3,10 @@ class Mozammil
 	public static void main(String[] args)
 	{
 		System.out.println("This file had been created by Mozammil, just for fun");
+	        m1();
+	 }
+	public static void m1()
+	{
+		System.out.println("This is m1() method");
+	}
 }
